59ed7f08d05e9657c89bc8113f3b22950982a129
Rellyson <rellysondouglas2015@gmail.com>
2020-08-05 20:51:18 -0300

    Getting TeacherList pages to list teacher of Proffy project | Added css responsive style

--- src/pages/TeachersList/index.tsx
+++ src/pages/TeachersList/index.tsx
@@ -3,26 +3,52 @@ import PageHeader from '../../components/PageHeader';
 
 import './styles.css'
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function TeachersList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
+                    <Select
+                        label="Matéria"
+                        name="subject"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Educação Física', label: 'Educação Física' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Sociologia', label: 'Sociologia' },
+                            { value: 'Filosofia', label: 'Filosofia' },
+                        ]}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana:</label>
-                        <input type="text" id="week_day" />
-                    </div>
+                    <Select
+                        label="Dia da Semana"
+                        name="week_day"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado-feira' },
+                        ]}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Input
+                        label="Hora"
+                        name="time"
+                        type="time"
+                    />
+
                 </form>
             </PageHeader>
 
