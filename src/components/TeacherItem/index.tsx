24364a827ee75074776a4dc6c07fabb078324913
Rellyson <rellysondouglas2015@gmail.com>
2020-08-06 08:59:32 -0300

    Getting data from backend | Exporting Teacher interface | Adding Props | Updating layout

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,37 +1,64 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../pages/services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+
+    function addNewConnection () {
+        api.post('connections', { user_id: teacher.id })
+    }
+    
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://pbs.twimg.com/profile_images/1259549879749226499/qnZpRyeY_400x400.jpg" alt="Diego Fernandes" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Diego Fernandes</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Entusiasta das melhores tecnologias de química avançada.
+                {teacher.bio}
+                {/* Entusiasta das melhores tecnologias de química avançada. 
             <br />
                 <br />
             Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000   pessoas já passaram por uma das minhas explosões.
-        </p>
+            */}
+            </p>
 
             <footer>
                 <p>
                     Preço/hora
-                <strong>R$ 80,00</strong>
+                <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                <button type="button">
+                <a 
+                    href={`https://wa.me/55${teacher.whatsapp}?text=Ol%C3%A1%2C+venho+do+Proffy%2C++queria+participar+das+suas+aulas%21`} 
+                    target="_blank"
+                    onClick={addNewConnection}
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
-                Entrar em contato
-            </button>
+
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     );
